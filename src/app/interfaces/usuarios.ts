d9610ca8e2659e00949ec3ca0e07cff000ad7390
ZarcoX <Ralexeyda@gmail.com>
2023-12-19 15:41:50 -0400

    pedro

--- src/app/interfaces/usuarios.ts
+++ src/app/interfaces/usuarios.ts
@@ -6,6 +6,6 @@ export interface usuarios {
 export const USUARIOS:usuarios[] = [
 
   {usuarios:'Zarco', password:'5c7c5220ef80a77c44277cc349b5d6b02768e358bd8f2423b342104711ab9a24'},
-  {usuarios:'Pedro', password:'5c7c5220ef80a77c44277cc349b5d6b02768e358bd8f2423b342104711ab9a24'},
+  {usuarios:'Pedro', password:'8b3d9636f67bbb48adb7101ff955f714332f884521e704b7cc862952668b3532'},
 
 ];
